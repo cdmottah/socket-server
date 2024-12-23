f27e4fec86c3d44396aafd28037d174348bd2025
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-12-23 05:18:22 -0500

    feat(admin-ui): integrate admin UI for monitoring and managing sockets
    Integrated the admin UI from https://admin.socket.io/#/ to monitor and manage the server. This tool allows us to see connected sockets, check for any down sockets, disconnect sockets, use username and password, join sockets to specific rooms, view namespaces, and more. It's a very useful tool for managing the server.
    
    Link to the class: https://platzi.com/home/clases/3276-socket-io/51170-admin-ui/

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
-const { teacherConections, studentsConections } = require("./routes")
-const express = require("express")
-const path = require("path")
-const { createServer } = require("http")
-const { Server } = require("socket.io")
+const { teacherConections, studentsConections } = require("./routes");
+const express = require("express");
+const path = require("path");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { instrument } = require('@socket.io/admin-ui');
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
 
+    }
+});
 
-app.use(express.static(path.join(__dirname, "views")))
+instrument(io, {
+    auth: false
+});
+
+app.use(express.static(path.join(__dirname, "views")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"))
-})
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
 
-const teachers = io.of("teachers");
-const students = io.of("students");
+const teachers = io.of("/teachers");
+const students = io.of("/students");
 
-teachers.on("connection", (socket) => teacherConections({ io: teachers, socket }))
-students.on("connection", (socket) => studentsConections({ io: students, socket }))
+teachers.on("connection", (socket) => {
+    console.log("Teacher connected:", socket.id);
+    teacherConections({ io: teachers, socket });
+});
 
+students.on("connection", (socket) => {
+    console.log("Student connected:", socket.id);
+    studentsConections({ io: students, socket });
+});
 
-
-httpServer.listen(3000)
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
